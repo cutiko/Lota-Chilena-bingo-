28c966c03360bd5ffea4f48dee648df3fdb37090
cutiko <cutiko@gmail.com>
2016-03-06 20:50:57 -0300

    FIX: correctly added titles in tabs

--- app/src/main/java/cl/cutiko/lotachilena/adapters/CurrentGameAdapter.java
+++ app/src/main/java/cl/cutiko/lotachilena/adapters/CurrentGameAdapter.java
@@ -1,9 +1,11 @@
 package cl.cutiko.lotachilena.adapters;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
+import cl.cutiko.lotachilena.R;
 import cl.cutiko.lotachilena.views.fragments.CallerFragment;
 import cl.cutiko.lotachilena.views.fragments.NumbersFragment;
 
@@ -12,8 +14,11 @@ import cl.cutiko.lotachilena.views.fragments.NumbersFragment;
  */
 public class CurrentGameAdapter extends FragmentPagerAdapter {
 
-    public CurrentGameAdapter(FragmentManager fm) {
+    private Context context;
+
+    public CurrentGameAdapter(FragmentManager fm, Context context) {
         super(fm);
+        this.context = context;
     }
 
     @Override
@@ -42,9 +47,9 @@ public class CurrentGameAdapter extends FragmentPagerAdapter {
     public CharSequence getPageTitle(int position) {
         switch (position) {
             case 0:
-                return "Lota";
+                return context.getString(R.string.game);
             case 1:
-                return "Números";
+                return context.getString(R.string.numbers);
         }
         return null;
     }
--- app/src/main/java/cl/cutiko/lotachilena/views/activities/BingoCallerActivity.java
+++ app/src/main/java/cl/cutiko/lotachilena/views/activities/BingoCallerActivity.java
@@ -31,7 +31,7 @@ public class BingoCallerActivity extends AppCompatActivity {
         getSupportActionBar().setTitle(gameName);
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
-        mSectionsPagerAdapter = new CurrentGameAdapter(getSupportFragmentManager());
+        mSectionsPagerAdapter = new CurrentGameAdapter(getSupportFragmentManager(), this);
 
         // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.container);
